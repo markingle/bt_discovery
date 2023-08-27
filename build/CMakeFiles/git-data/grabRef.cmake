4b2ac0a22f45fd7f0a403d6c7380fdd4e7b0bce5
mark.ingle@coporium.com <mark.ingle@coporium.com>
2023-08-26 23:26:16 -0400

    rssi function - WIP

--- build/CMakeFiles/git-data/grabRef.cmake
+++ build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "/Users/markbingle/Downloads/IOTCode/Classic_BT_Demo/bt_discovery/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "/Users/markbingle/esp/esp-idf/.git")
+set(GIT_DIR "/Users/markbingle/Downloads/IOTCode/Classic_BT_Demo/bt_discovery/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
--- main/main.c
+++ main/main.c
@@ -25,8 +25,20 @@
 #include "esp_bt_main.h"
 #include "esp_bt_device.h"
 #include "esp_gap_bt_api.h"
+#include "driver/uart.h"
+#include "freertos/queue.h"
 
 #define GAP_TAG          "GAP"
+
+#define EX_UART_NUM UART_NUM_0
+#define PATTERN_CHR_NUM    (3)         /*!< Set the number of consecutive and identical characters received by receiver which defines a UART pattern*/
+
+#define BUF_SIZE (1024)
+#define RD_BUF_SIZE (BUF_SIZE)
+static QueueHandle_t uart0_queue;
+
+static const char *TAG = "uart_events";
+
 
 typedef enum {
     APP_GAP_STATE_IDLE = 0,
@@ -49,57 +61,6 @@ typedef struct {
 } app_gap_cb_t;
 
 static app_gap_cb_t m_dev_info;
-
-static char *bda2str(esp_bd_addr_t bda, char *str, size_t size)
-{
-    if (bda == NULL || str == NULL || size < 18) {
-        return NULL;
-    }
-
-    uint8_t *p = bda;
-    sprintf(str, "%02x:%02x:%02x:%02x:%02x:%02x",
-            p[0], p[1], p[2], p[3], p[4], p[5]);
-    return str;
-}
-
-static char *bdrmtname2str(uint8_t *rmtname, char *str, size_t size)
-{
-    if (rmtname == NULL || str == NULL || size < ESP_BT_GAP_READ_REMOTE_NAME_EVT) {
-        return NULL;
-    }
-
-    //uint8_t *p = rmtname;
-    //sprintf(str, "%02x:%02x:%02x:%02x:%02x:%02x",
-    //        p[0], p[1], p[2], p[3], p[4], p[5]);
-    
-    return str = ((char *)rmtname);
-}
-
-// ********************* THIS FUNCTION IS NOT NEEDED YET **********************
-
-/*static char *uuid2str(esp_bt_uuid_t *uuid, char *str, size_t size)
-{
-    if (uuid == NULL || str == NULL) {
-        return NULL;
-    }
-
-    if (uuid->len == 2 && size >= 5) {
-        sprintf(str, "%04x", uuid->uuid.uuid16);
-    } else if (uuid->len == 4 && size >= 9) {
-        sprintf(str, "%08"PRIx32, uuid->uuid.uuid32);
-    } else if (uuid->len == 16 && size >= 37) {
-        uint8_t *p = uuid->uuid.uuid128;
-        sprintf(str, "%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x",
-                p[15], p[14], p[13], p[12], p[11], p[10], p[9], p[8],
-                p[7], p[6], p[5], p[4], p[3], p[2], p[1], p[0]);
-    } else {
-        return NULL;
-    }
-
-    return str;
-}*/
-
-
 
 static bool get_name_from_eir(uint8_t *eir, uint8_t *bdname, uint8_t *bdname_len)
 {
@@ -131,6 +92,43 @@ static bool get_name_from_eir(uint8_t *eir, uint8_t *bdname, uint8_t *bdname_len
     }
 
     return false;
+}
+
+static void bt_app_gap_init(void)
+{
+    app_gap_cb_t *p_dev = &m_dev_info;
+    memset(p_dev, 0, sizeof(app_gap_cb_t));
+
+    p_dev->state = APP_GAP_STATE_IDLE;
+}
+
+static char *bda2str(esp_bd_addr_t bda, char *str, size_t size)
+{
+    if (bda == NULL || str == NULL || size < 18) {
+        return NULL;
+    }
+
+    uint8_t *p = bda;
+    sprintf(str, "%02x:%02x:%02x:%02x:%02x:%02x",
+            p[0], p[1], p[2], p[3], p[4], p[5]);
+    return str;
+}
+
+static char *parsevalue(char *data_string)
+
+{
+    char str[30];
+    char * pch;
+    sprintf(str, "%s",data_string);
+    ESP_LOGI(TAG, "New String: %s", str);
+    pch = strtok(str,"=");
+    while (pch != NULL)
+        {
+            ESP_LOGI(TAG, "PCH!!!: %s", pch);
+            pch = strtok (NULL, "=");
+            data_string = pch;
+        }
+    return data_string;
 }
 
 static void update_device_info(esp_bt_gap_cb_param_t *param)
@@ -172,7 +170,6 @@ static void update_device_info(esp_bt_gap_cb_param_t *param)
         }
     }
 
-    /* search for device with Major device type "PHONE" or "Audio/Video" in COD */
     app_gap_cb_t *p_dev = &m_dev_info;
     if (p_dev->dev_found) {
         return;
@@ -211,12 +208,13 @@ static void update_device_info(esp_bt_gap_cb_param_t *param)
     //esp_bt_gap_cancel_discovery();
 }
 
-static void bt_app_gap_init(void)
+static char *bdrmtname2str(uint8_t *rmtname, char *str, size_t size)
 {
-    app_gap_cb_t *p_dev = &m_dev_info;
-    memset(p_dev, 0, sizeof(app_gap_cb_t));
-
-    p_dev->state = APP_GAP_STATE_IDLE;
+    if (rmtname == NULL || str == NULL || size < ESP_BT_GAP_READ_REMOTE_NAME_EVT) {
+        return NULL;
+    }
+    
+    return str = ((char *)rmtname);
 }
 
 static void bt_app_gap_cb(esp_bt_gap_cb_event_t event, esp_bt_gap_cb_param_t *param)
@@ -302,8 +300,134 @@ static void bt_app_gap_start_up(void)
     /* start to discover nearby Bluetooth devices */
     app_gap_cb_t *p_dev = &m_dev_info;
     p_dev->state = APP_GAP_STATE_DEVICE_DISCOVERING;
-    esp_bt_gap_start_discovery(ESP_BT_INQ_MODE_GENERAL_INQUIRY, 30, 0);
+    //esp_bt_gap_start_discovery(ESP_BT_INQ_MODE_GENERAL_INQUIRY, 30, 0);
 }
+
+
+
+static void uart_event_task(void *pvParameters)
+{
+    uart_event_t event;
+    size_t buffered_size;
+    uint8_t* dtmp = (uint8_t*) malloc(RD_BUF_SIZE);
+    uint8_t * data = (uint8_t *) malloc(BUF_SIZE);
+    char str[20];
+    char * cmd;
+    char * param;
+
+    for(;;) {
+        //Waiting for UART event.
+        if(xQueueReceive(uart0_queue, (void * )&event, (TickType_t)portMAX_DELAY)) {
+            bzero(dtmp, RD_BUF_SIZE);
+            ESP_LOGI(TAG, "uart[%d] event:", EX_UART_NUM);
+            switch(event.type) {
+                //Event of UART receving data
+                /*We'd better handler data event fast, there would be much more data events than
+                other types of events. If we take too much time on data event, the queue might
+                be full.*/
+                case UART_DATA:
+                    int len = uart_read_bytes(EX_UART_NUM, data, (BUF_SIZE - 1), 100/portTICK_PERIOD_MS);
+                    data[len] = '\0';
+                        //bt_app_gap_start_up();
+                    sprintf(str, "%s", (char *) data);
+                    //ESP_LOGI(TAG, "New String: %s", str);
+                    cmd = strtok(str,"=");
+                    param = strtok(NULL,"=");
+                    ESP_LOGI(TAG, "Command: %s", cmd);
+                    ESP_LOGI(TAG, "Parameter: %s", param);
+                    if (strcmp((char*)cmd, "rssi") == 0)
+                        {
+                           ESP_LOGI(TAG, "Set RSSI");
+                        }
+                    if (cmd == 'btaddr')
+                        {
+                            ESP_LOGI(TAG, "Set BT ADDRESS");
+                        }
+                    ESP_LOGI(TAG, "YoYO!!!");
+                //Event of HW FIFO overflow detected
+                case UART_FIFO_OVF:
+                    ESP_LOGI(TAG, "hw fifo overflow");
+                    // If fifo overflow happened, you should consider adding flow control for your application.
+                    // The ISR has already reset the rx FIFO,
+                    // As an example, we directly flush the rx buffer here in order to read more data.
+                    uart_flush_input(EX_UART_NUM);
+                    xQueueReset(uart0_queue);
+                    break;
+                //Event of UART ring buffer full
+                case UART_BUFFER_FULL:
+                    ESP_LOGI(TAG, "ring buffer full");
+                    // If buffer full happened, you should consider increasing your buffer size
+                    // As an example, we directly flush the rx buffer here in order to read more data.
+                    uart_flush_input(EX_UART_NUM);
+                    xQueueReset(uart0_queue);
+                    break;
+                //Event of UART RX break detected
+                case UART_BREAK:
+                    ESP_LOGI(TAG, "uart rx break");
+                    break;
+                //Event of UART parity check error
+                case UART_PARITY_ERR:
+                    ESP_LOGI(TAG, "uart parity error");
+                    break;
+                //Event of UART frame error
+                case UART_FRAME_ERR:
+                    ESP_LOGI(TAG, "uart frame error");
+                    break;
+                //UART_PATTERN_DET
+                /*case UART_PATTERN_DET:
+                    uart_get_buffered_data_len(EX_UART_NUM, &buffered_size);
+                    int pos = uart_pattern_pop_pos(EX_UART_NUM);
+                    ESP_LOGI(TAG, "[UART PATTERN DETECTED] pos: %d, buffered size: %d", pos, buffered_size);
+                    if (pos == -1) {
+                        // There used to be a UART_PATTERN_DET event, but the pattern position queue is full so that it can not
+                        // record the position. We should set a larger queue size.
+                        // As an example, we directly flush the rx buffer here.
+                        uart_flush_input(EX_UART_NUM);
+                    } else {
+                        uart_read_bytes(EX_UART_NUM, dtmp, pos, 100 / portTICK_PERIOD_MS);
+                        uint8_t pat[PATTERN_CHR_NUM + 1];
+                        memset(pat, 0, sizeof(pat));
+                        uart_read_bytes(EX_UART_NUM, pat, PATTERN_CHR_NUM, 100 / portTICK_PERIOD_MS);
+                        ESP_LOGI(TAG, "YOU ARE NOW IN A MODE!!!  MORE TO COME...");
+                        ESP_LOGI(TAG, "read data: %s", dtmp);
+                        ESP_LOGI(TAG, "read pat : %s", pat);
+                    }
+                    break;*/
+                //Others
+                default:
+                    ESP_LOGI(TAG, "uart event type: %d", event.type);
+                    break;
+            }
+        }
+    }
+    free(dtmp);
+    dtmp = NULL;
+    vTaskDelete(NULL);
+}
+
+// ********************* THIS FUNCTION IS NOT NEEDED YET **********************
+
+/*static char *uuid2str(esp_bt_uuid_t *uuid, char *str, size_t size)
+{
+    if (uuid == NULL || str == NULL) {
+        return NULL;
+    }
+
+    if (uuid->len == 2 && size >= 5) {
+        sprintf(str, "%04x", uuid->uuid.uuid16);
+    } else if (uuid->len == 4 && size >= 9) {
+        sprintf(str, "%08"PRIx32, uuid->uuid.uuid32);
+    } else if (uuid->len == 16 && size >= 37) {
+        uint8_t *p = uuid->uuid.uuid128;
+        sprintf(str, "%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x",
+                p[15], p[14], p[13], p[12], p[11], p[10], p[9], p[8],
+                p[7], p[6], p[5], p[4], p[3], p[2], p[1], p[0]);
+    } else {
+        return NULL;
+    }
+
+    return str;
+}*/
 
 void app_main(void)
 {
@@ -338,5 +462,43 @@ void app_main(void)
         return;
     }
 
-    bt_app_gap_start_up();
+    if ((ret = esp_bt_dev_set_device_name("PC_DONGLE")) != ESP_OK) {
+        ESP_LOGE(GAP_TAG, "%s initialize device name failed: %s\n", __func__, esp_err_to_name(ret));
+        return;
+    }
+
+    if ((ret = esp_bredr_tx_power_set(ESP_PWR_LVL_N0, ESP_PWR_LVL_N0)) != ESP_OK) {
+        ESP_LOGE(GAP_TAG, "%s set RSSI power level failed: %s\n", __func__, esp_err_to_name(ret));
+        return;
+    }
+
+   // bt_app_gap_start_up();
+
+    esp_log_level_set(TAG, ESP_LOG_INFO);
+
+    /* Configure parameters of an UART driver,
+     * communication pins and install the driver */
+    uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_DEFAULT,
+    };
+    //Install UART driver, and get the queue.
+    uart_driver_install(EX_UART_NUM, BUF_SIZE * 2, BUF_SIZE * 2, 20, &uart0_queue, 0);
+    uart_param_config(EX_UART_NUM, &uart_config);
+
+    //Set UART log level
+    esp_log_level_set(TAG, ESP_LOG_INFO);
+    //Set UART pins (using UART0 default pins ie no changes.)
+    uart_set_pin(EX_UART_NUM, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+
+    //USE THIS FUNCTION TO MAKE WAY FOR SEPARATE MODES NEEDED ON THE DONGLE
+    uart_enable_pattern_det_baud_intr(EX_UART_NUM, '+', PATTERN_CHR_NUM, 9, 0, 0);
+    //Reset the pattern queue length to record at most 20 pattern positions.
+    uart_pattern_queue_reset(EX_UART_NUM, 20);
+
+    xTaskCreate(uart_event_task, "uart_event_task", 2048, NULL, 12, NULL);
 }
